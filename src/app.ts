903ecf6d96a027ea4ddb8839ccf8c567c303c0fd
Saulo Assef Alves <saulo.assef98@gmail.com>
2025-03-14 22:25:11 -0300

    atualizar custom fields, criar task e criar note

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import tintimWebhookRoutes from "./routes/tintimWebhookRoutes";
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", tintimWebhookRoutes);
+app.use("/tintimWebhook", tintimWebhookRoutes);
+
+console.log("🚀 Server started");
+
 export default app;
--- src/routes/tintimWebhookRoutes.ts
+++ src/routes/tintimWebhookRoutes.ts
@@ -1,17 +1,48 @@
 import { Router, Request, Response } from "express";
+import { ClienteController } from "../controllers/cliente.controller";
+import { ClienteModel } from "../models/cliente.models";
+import { TintimWebhookController } from "../controllers/tintimWebhook.controller";
+import { getClientesTintim } from "../config/database";
 
 const router = Router();
 
-router.post("/tintimWebhook", (req: Request, res: Response) => {
-  try {
-    const webhookData = req.body;
 
-    console.log("📩 Webhook recebido:", JSON.stringify(webhookData, null, 2));
+const clientes = await getClientesTintim();
+
+const cliente = new ClienteModel(
+  "saulotestes",
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjZmZWE4ZTQ0YTkyNzliNWMyNTZlZTcwMGE1N2Q3ZTQ1MDA1NTQ5ODU2MTU4ZDY5NjAwODAxNWU2MzYzNTc5N2EyMTFiNGZkMGQwMmVkNTVlIn0.eyJhdWQiOiJiMDM3ZGNhMC1jOTBkLTQ1ZDctYWIzZC01ZTEwMzYyMGMzNDQiLCJqdGkiOiI2ZmVhOGU0NGE5Mjc5YjVjMjU2ZWU3MDBhNTdkN2U0NTAwNTU0OTg1NjE1OGQ2OTYwMDgwMTVlNjM2MzU3OTdhMjExYjRmZDBkMDJlZDU1ZSIsImlhdCI6MTc0MTgwMTg2NywibmJmIjoxNzQxODAxODY3LCJleHAiOjE3NDMxMjAwMDAsInN1YiI6IjEyNzYxMDc1IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjM0MzAwMjU1LCJiYXNlX2RvbWFpbiI6ImtvbW1vLmNvbSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiNDYzYjE1ZTctNjQxYy00Nzk3LWJhNTYtYTcwNTUxMjExYmUyIiwiYXBpX2RvbWFpbiI6ImFwaS1jLmtvbW1vLmNvbSJ9.lj1qi7Vjv9jOp_ukScYoQUr4cnBggciLar9RWd_p3BqH9eRqSxlP3X1YoN-QE20hGNFULtW8ywAWRvTmbN8QXi1bVw6qhDKNb582KftoVU828JUapJBF432u2COpkqXNhkGOJ3lM6gPEE-4UCcyQfigSbnY2acBRytysKRS7bUOvj6sQwepPsJ5ekfw1sg6nWEkxYeAIsG8skVB_ljkO_NY5-TEBR0day8Wbyzb5qmF9-Eu7HSVsdYALohspgwpAMGXi2F7CbngIdvHqQQjJJy9EvNAAZS6yJQC_JSqjwQ5ANnngxULmpUZsHn7EFJd1IWJ7Slfda-gRqY5nxItXBQ"
+);
+
+clientes.forEach((cliente) => {
+  const clienteModel = new ClienteModel(cliente.nome, cliente.token);
+  console.log("🔍 Cliente:", cliente.nome);
+  router.post(`/${cliente.nome}`, async (req: Request, res: Response) => {
+    try {
+      // await new TintimWebhookController(clienteModel).atualizarFiledsWebhookTintim(
+      //   req,
+      //   res
+      // );
+
+      res.status(200).json({ message: "✅ Webhook recebido com sucesso!!!!" });
+    } catch (error) {
+      console.error("❌ Erro ao buscar clientes:", error);
+      res.status(500).json({ error: "Erro ao buscar clientes" });
+    }
+  });
+});
+
+router.post("/clientes", async (req: Request, res: Response) => {
+  try {
+    await new TintimWebhookController(cliente).atualizarFiledsWebhookTintim(
+      req,
+      res
+    );
 
     res.status(200).json({ message: "✅ Webhook recebido com sucesso!" });
   } catch (error) {
-    console.error("❌ Erro ao processar webhook:", error);
-    res.status(500).json({ error: "Erro ao processar webhook" });
+    console.error("❌ Erro ao buscar clientes:", error);
+    res.status(500).json({ error: "Erro ao buscar clientes" });
   }
 });
 

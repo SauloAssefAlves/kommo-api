f41d8eea88ee2663917c3bae7ee1fbf483687b45
Saulo Assef Alves <saulo.assef98@gmail.com>
2025-03-18 12:44:22 -0300

    teste

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import tintimWebhookRoutes from "./routes/tintimWebhookRoutes";
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/tintimWebhook", tintimWebhookRoutes);
+
+app.get("/teste", (req, res) => {
+  res.send("🚀 Servidor rodando!");
+});
 
 console.log("🚀 Server started");
 
